ee6b98dba2af3927246279e6045f4fbee82ec6ce
fald <4348649+fald@users.noreply.github.com>
2023-05-11 15:21:53 -0400

    Node class

---
+++ decision_trees/Node.py
@@ -0,0 +1,13 @@
+from pandas import DataFrame
+
+class Node:
+    # A node for a binary decision tree
+    def __init__(self, feature = None, threshold = None, left: 'Node' = None, right: 'Node' = None, *, value=None) -> None:
+        # self.data = data # Storing subsets of data saves minor compute at cost of relatively large memory, and is only useful during training, so.
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        # If it's a leaf node, replace with most common class.
+        self.value = None
+        
